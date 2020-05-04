e109afef3396edb2e1e69e2064344bc38cbba3ae
Alenilson Souza <alenilsonsouza@gmail.com>
2020-05-04 00:38:07 -0300

    dashboard

--- views/painel/panel.php
+++ views/painel/panel.php
@@ -7,10 +7,20 @@
     <div class="col-sm-3">
         <div class="card">
             <div class="card-body">
+                <h5 class="card-title">Movimentação</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Gerencie transações</h6>
+                <p class="card-text">--</p>
+                <a href="<?=BASE;?>painel_movimentacao" class="btn btn-primary">Acessar</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="card">
+            <div class="card-body">
                 <h5 class="card-title">Usuários</h5>
                 <h6 class="card-subtitle mb-2 text-muted">Gerencie usuários</h6>
-                <p class="card-text">0 registros</p>
-                <a href="#" class="btn btn-primary">Acessar</a>
+                <p class="card-text">--</p>
+                <a href="<?=BASE;?>painel_user" class="btn btn-primary">Acessar</a>
             </div>
         </div>
     </div>
